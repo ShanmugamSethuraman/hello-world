b78b7d1cb06a2744c91a1af56d870dbded49c735
Shanmugam Sethuraman <54280129+ShanmugamSethuraman@users.noreply.github.com>
2019-08-20 17:52:25 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,33 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,g
+
+fname = ""
+sname = ""
 
 app = Flask(__name__)
 
 @app.route('/')
 def shan():
     # return "shan"
-    return render_template("flamesForm.html")
-    print("test01")
+    g.homepage = render_template("flamesForm.html")
+    return g.homepage
 @app.route('/' , methods = ["POST"])
 def getvalue():
-    fname = request.form['YourName']
-    sname = request.form['PartnerName']
-    print(fname, sname)
-    return fname,sname
+    g.fname = request.form['YourName']
+    g.sname = request.form['PartnerName']
+    print(g.fname, g.sname)
+    return g.fname+g.sname
 
+def runapp():
+    print("printapp entry")
+    app.run()
+    print("printapp exit")
+    return
 
-app.run(debug=True)
+def main():
+    print("entry")
+    runapp()
+    # testfn = request.form['YourName']
+    print(testfn)
+    print("test00111")
+
+main()
